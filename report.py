7c4d3ef838d2b6b648e4ed1880366b6d09e92f50
Rutvij Jethwa <rutvijjethwa@gmail.com>
2017-01-16 12:38:21 +0530

    Update report.py

--- report.py
+++ report.py
@@ -1,19 +1,35 @@
 from pprint import pprint
 import requests
+import os.path
+import time
+import pandas as pd
 
-
-def getReportf(publicKey, resourceID):
+def getReportf(publicKey, resourceID,filename):
     dataSet = {'apikey': publicKey, 'resource': resourceID}
     reportHeaders = {
         "Accept-Encoding": "gzip, deflate",
         "User-Agent": "gzip,  Holly Mother Of Godzilla"
     }
-    response = requests.get('https://www.virustotal.com/vtapi/v2/file/report', params=dataSet, headers=reportHeaders)
-    json_response = response.json()
+    while True:
+        try:
+            response = requests.get('https://www.virustotal.com/vtapi/v2/file/report', params=dataSet, headers=reportHeaders)
+            json_response = response.json()
+            if json_response.get('verbose_msg') == 'Scan finished, information embedded':
+                break
+            else:
+                time.sleep(10)
+        except:
+            continue
     print("####################################################")
-    pprint(json_response)
+    fname = os.path.basename(filename)
+    df = pd.DataFrame(json_response)
+    writer = pd.ExcelWriter('{}_report.xlsx'.format(fname))
+    df.to_excel(writer)
+    writer.save()
+    print('Report Saved to : {}'.format(os.getcwd()))
 
-def getReportu(publicKey, resourceID):
+
+def getReportu(publicKey, resourceID,filename):
     reportHeaders = {
         "Accept-Encoding": "gzip, deflate",
         "User-Agent": "gzip,  Holly Mother Of Godzilla"

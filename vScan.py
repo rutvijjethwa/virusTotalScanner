54752af4ee31677f88ae142947bb67d61bfb4c16
Rutvij Jethwa <rutvijjethwa@gmail.com>
2017-01-16 12:16:18 +0530

    Export Scan result to Excel File

--- vScan.py
+++ vScan.py
@@ -3,6 +3,7 @@ from configparser import ConfigParser
 from fileModule import fileScanner
 from urlModule import urlScanner
 import report
+import time
 
 configReader = ConfigParser()
 configReader.read(filenames='config.ini')
@@ -20,9 +21,10 @@ if sys.argv[1] == '-h':
     """ )
 elif sys.argv[1] == '-f':
     reportID = fileScanner(fileLocation=sys.argv[2], publicKey=key)
-    report.getReportf(key.get('apikey'), reportID)
+    print('Waiting for response...')
+    report.getReportf(key.get('apikey'), reportID, sys.argv[2])
 elif sys.argv[1] == '-u':
     reportID = urlScanner(sys.argv[2], key.get('apikey'))
-    report.getReportu(key.get('apikey'), reportID)
+    report.getReportu(key.get('apikey'), reportID,sys.argv[2])
 else:
     print("INVALID INPUT")
